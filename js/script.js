afb1be86bff3b79ef717e914127e8861dbbd3055
thaiikhang <huynhkhang690@gmail.com>
2023-10-22 18:53:01 +0700

    [Animation] Added animation to sections

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// // init AOS animation
-// AOS.init({
-//   duration: 1000,
-//   offset: 100,
-// });
+// init AOS animation
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
 
 const trendingSushis = [
   "Make Sushi",
